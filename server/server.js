4e6bb8d763a1bd802db6ecf70d4e39545d19d3e4
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-05-15 10:56:19 -0400

    updated server with routes

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
 const mongoose = require('mongoose')
+const eventRoutes = require('./routes/events')
+const userRoutes = require('./routes/user')
 
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 app.use((req, res, next) => {
@@ -12,6 +15,7 @@ app.use((req, res, next) => {
 })
 app.use('/api/events', eventRoutes)
 app.use('/api/user', userRoutes)
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {

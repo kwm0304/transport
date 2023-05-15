4d7e415897b2ca57ee5d7f482359fe26e61833b3
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-05-15 10:52:20 -0400

    added routes

---
+++ server/routes/events.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const { 
+  createEvent,
+  getEvents,
+  getEvent,
+  deleteEvent,
+  updateEvent
+} = require('../controllers/eventController')
+const requireAuth = require('../middleware/requireAuth')
+const router = express.Router()
+
+router.use(requireAuth)
+router.get('/', getEvents)
+router.get('/:id', getEvent)
+router.post('/', createEvent)
+router.delete('/:id', deleteEvent)
+router.patch('/:id', updateEvent)
+
+module.exports = router
---
+++ server/routes/user.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const { signupUser, loginUser } = require('../controllers/userController')
+const router = express.Router()
+router.post('/login', loginUser)
+router.post('/signup', signupUser)
+module.exports = router

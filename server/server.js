b115dd27b217571486d1a1446556f25f7412b0a2
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-05-27 13:34:55 -0400

    updated server/server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const eventRoutes = require('./routes/events')
 const userRoutes = require('./routes/user')
+const phonebookRoutes = require('./routes/phonebook')
 
 const app = express()
 
@@ -15,6 +16,7 @@ app.use((req, res, next) => {
 })
 app.use('/api/events', eventRoutes)
 app.use('/api/user', userRoutes)
+app.use('/api/contacts', phonebookRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {

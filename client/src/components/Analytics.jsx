8868c12b89701fa5cc20bcf3bb6f81e82105fdfb
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-06-04 19:18:59 -0400

    set up tab and piechart library in analytics

--- client/src/components/Analytics.jsx
+++ client/src/components/Analytics.jsx
@@ -2,6 +2,10 @@ import { useEffect } from 'react'
 import { useAuthContext } from '../hooks/useAuth'
 import { useExpenseContext } from '../hooks/useExpenseContext'
 import { useEventContext } from '../hooks/useEventContext'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
+import 'react-tabs/style/react-tabs.css';
+import { PieChart } from 'react-minimal-pie-chart'
+
 
 const Analytics = () => {
   const { user } = useAuthContext()
@@ -38,7 +42,34 @@ const Analytics = () => {
   return (
     <>
     <h2 className="text-center text-blue-900 font-bold text-2xl my-8">Analytics</h2>
-
+    <Tabs>
+      <TabList>
+        <Tab >Expenses</Tab>
+        <Tab>Revenues</Tab>
+      </TabList>
+      <TabPanel >
+        <div className='bg-blue-900 text-white'>
+          <Tabs>
+            <TabList>
+              <Tab>Week</Tab>
+              <Tab>Month</Tab>
+              <Tab>Year</Tab>
+            </TabList>
+          </Tabs>
+        </div>
+      </TabPanel>
+      <TabPanel>
+      <div className='bg-blue-900 text-white'>
+          <Tabs>
+            <TabList>
+              <Tab>Week</Tab>
+              <Tab>Month</Tab>
+              <Tab>Year</Tab>
+            </TabList>
+          </Tabs>
+        </div>
+      </TabPanel>
+    </Tabs>
     </>
   )
 }
